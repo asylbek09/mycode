61c6affe13ae48a1dbd65e70e4ef7b291ad125b9
asylbek09 <asylbek09@gmail.com>
2021-03-30 15:00:21 +0000

    update dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-char_name = input("Which character do you want to know about? (Wolverine, Harry Potter, Agent Fitz or -1 to EXIT)")
+char_name = input("Which character do you want to know about? (Wolverine, Harry Potter, Agent Fitz)")
 char_stat = input("What statistic do you want to know about? (real name, powers, archenemy)")
 
-heroes = {
+name = char_name.lower()
+stat = char_stat.lower()
+
+heroes=  {
 "wolverine":
     {"real name": "James Howlett",
     "powers": "regeneration",
@@ -16,12 +19,8 @@ heroes = {
     {"real name": "Leopold Fitz",
     "powers": "intelligence",
     "archenemy": "Hydra",}
-        }
+}
 
-while char_name != -1:
-    name = char_name.lower()
-    stat = char_stat.lower()
-
-    print(name.capitalize() + "'s " + stat.capitalize() + " is: " + heroes[name][stat])
-     continue
+print(f"{name.capitalize()}'s {stat.capitalize()} is: {heroes[name][stat]}")
+#print(name.capitalize() + "'s " + stat.capitalize() + " is: " + heroes[name][stat])
 
