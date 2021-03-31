3cf5f44bb4f91cbfe1de54b71f978225b63d1169
asylbek09 <asylbek09@gmail.com>
2021-03-31 06:10:04 -0400

    guess capital game

---
+++ countries/guesscapital.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import json
+import random
+
+def getCapitals():
+
+    capitals_json = open ('C:/Users/asylb/OneDrive/Documents/TLG/Python/mycode/countries/capitals.json', "r")
+
+    capitals_dict = json.load(capitals_json)
+
+    return capitals_dict
+
+def prompt():
+    capitals = getCapitals()
+    randCountry = random.choice(capitals)
+    print(randCountry)
+
+    client_choice = input("What is the capital of: " + randCountry['country'] + "? ")
+
+    if client_choice.lower() == randCountry['capital'].lower():
+        print(f"Congratulations, {randCountry['country']}'s capital is {randCountry['capital']}")
+    else:
+        print("Try again!")
+
+def main():
+    prompt()
+
+main()
